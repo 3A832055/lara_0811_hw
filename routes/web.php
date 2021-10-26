8f0de90660cce2683d724011bd353518c9849dee
蔡羽宣 <3a832055@gm.student.ncut.edu.tw>
2021-10-26 21:15:50 +0800

    設定Route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home',[HomeController::class,'index'])->name('home.index');
+Route::get('about',[HomeController::class,'index'])->name('about.index');
+Route::get('about',function () {
+    return view ('about');
+});
+Route::get('news',[HomeController::class,'index'])->name('news.index');
+Route::get('news',function () {
+    return view ('news');
+});
+
